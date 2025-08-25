6bf50b1209806d9ea3e76b84fe1078570b4e8f97
Carlos Delgadillo <cardel91@gmail.com>
2025-08-25 13:39:28 -0600

    Word game with useState

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 import "./index.css"
+import { ScrambleWords } from './05-useReducer/ScrambleWords'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { PokemonPage } from './03-examples/PokemonPage'
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { TasksApp } from './05-useReducer/TaskApp'
+// import { TasksApp } from './05-useReducer/TaskApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TasksApp />
+    <ScrambleWords />
   </StrictMode>,
 )
